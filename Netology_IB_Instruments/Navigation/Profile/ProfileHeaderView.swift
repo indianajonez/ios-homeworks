d4dc9192fb80521f6c81fc4b2f0efe75d33e7d5d
Indiana Jonez <indiana.jonez@yandex.ru>
2023-04-02 21:35:33 +0300

    2 iteration

--- Netology_IB_Instruments/Navigation/Profile/ProfileHeaderView.swift
+++ Netology_IB_Instruments/Navigation/Profile/ProfileHeaderView.swift
@@ -82,7 +82,7 @@ class ProfileHeaderView: UIView {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented") // зачем эта штука?
+        fatalError("init(coder:) has not been implemented") 
     }
     
     private func layout() {
@@ -96,7 +96,7 @@ class ProfileHeaderView: UIView {
             fullNameLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 27),
             fullNameLabel.leadingAnchor.constraint(equalTo: avatarImage.trailingAnchor),
             fullNameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-            fullNameLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+//            fullNameLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
             
             statusLabel.topAnchor.constraint(equalTo: fullNameLabel.bottomAnchor, constant: 16),
             statusLabel.leadingAnchor.constraint(equalTo: avatarImage.trailingAnchor, constant: 20),
--- Netology_IB_Instruments/Navigation/Profile/ProfileViewController.swift
+++ Netology_IB_Instruments/Navigation/Profile/ProfileViewController.swift
@@ -9,24 +9,86 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    private lazy var profileHeaderView = ProfileHeaderView()
+    // почему не добавляем fileprivate let data =  ?
+    // почему не используем private lazy var tableView: UITableView = {
+    // let tableView = UITableView.init(
+    // frame: .zero
+    // style: .plain
+    //)
+    //tableView.translatesAutoresizingMaskIntoConstraints = false
+    
+    // return tableView
+    // }()
+    
+    private lazy var table: UITableView = {
+        let table = UITableView(frame: .zero, style: .plain)
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.delegate = self
+        table.dataSource = self
+        table.register(PostTableViewCell.self, forCellReuseIdentifier: PostTableViewCell.identifier) //регистрация ячейки для переиспользования
+        
+        return table
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
-        self.title = "Profile"
-        view.addSubview(profileHeaderView)
-        profileHeaderView.frame = view.safeAreaLayoutGuide.layoutFrame
         layout()
         
     }
     
     private func layout() {
+        
+        view.addSubview(table)
+        
         NSLayoutConstraint.activate([
-            profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            profileHeaderView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            profileHeaderView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            profileHeaderView.heightAnchor.constraint(equalToConstant: 220)
+            table.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            table.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            table.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            table.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
+}
+
+// MARK: - UITableViewDelegate
+extension ProfileViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        UITableView.automaticDimension
+    } // auto math height size for cell
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = ProfileHeaderView()
+        header.backgroundColor = .darkGray
+        return section == 0 ? header : nil
+        //nil
+    }
+}
+
+
+// MARK: - UITableViewDataSource дата сорс отвечает за то, чтобы наполнить наблицу данными
+//tableView(_:titleForHeaderIn Section:)
+//tableView(_:titleForFooterIn Section:)
+extension ProfileViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        section == 0 ? 0 : listPost.count // с помощью этого метода указываем кол-во ячеек
+        //listPost.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PostTableViewCell.identifier) as? PostTableViewCell else { return UITableViewCell()}
+        cell.setupCell(listPost[indexPath.row])
+        //cell.selectionStyle = .none
+        return cell
+    }
+}
+
+// MARK: - UIView
+
+extension UIView {
+    static var identifier: String {return String(describing: self)}
 }
